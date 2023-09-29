0a320edb85e526a48fed205012150b48bd6540fc
Mohammad Tariq <fk68790@gmail.com>
2023-09-29 18:53:27 +0530

    db

--- models/db.js
+++ models/db.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: process.env.DIALECT,
-    host: process.env.HOST,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.MYSQL_PORT,
-    logging: false,
-});
-
+// const sequelize = new Sequelize({
+//     dialect: process.env.DIALECT,
+//     host: process.env.HOST,
+//     username: process.env.USERNAME,
+//     password: process.env.PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.MYSQL_PORT,
+//     logging: false,
+// });
+const sequelize = new Sequelize(process.env.MYSQL_URI, { logging: false });
 module.exports = sequelize;
